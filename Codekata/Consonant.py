3c5b4adfdc2a08b5fdcb67d0abff4188cdf0f31b
Radhika-M <51691238+Radhika-M@users.noreply.github.com>
2019-06-12 12:27:18 +0530

    Create Consonant.py

---
+++ Codekata/Consonant.py
@@ -0,0 +1,9 @@
+m=input()
+vowels=["a","e","i","o","u"]
+consonant=["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"]
+if m in vowels:
+  print("Vowel")
+elif m in consonant:
+  print("Consonant")
+else:
+  print("Invalid")  

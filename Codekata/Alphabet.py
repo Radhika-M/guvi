309b0e7f5347ec8f0681dd843ddb065fc894a7e6
Radhika-M <51691238+Radhika-M@users.noreply.github.com>
2019-06-12 12:45:38 +0530

    Create Alphabet.py

---
+++ Codekata/Alphabet.py
@@ -0,0 +1,6 @@
+m=input()
+alphapet=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+if m in alphapet:
+  print("Alphabet")
+else:
+  print("No")

dc465f1a039f372b5d53d37bcd19a51045b60739
Radhika-M <51691238+Radhika-M@users.noreply.github.com>
2019-06-13 16:08:08 +0530

    Update Prime.py

--- Codekata/Prime.py
+++ Codekata/Prime.py
@@ -1,8 +1,9 @@
 m=int(input())
 if m>1:
     for i in range(2,m):
-      if(m%i)==0:
-        print("no")
+        if(m%i)==0:
+            print("no")
+            break
     else:
       print("yes")
 else:
